33fc0a362be7cc8e247937892d789feec5181c97
chayanon <chayanon.mongkonhatairat@gmail.com>
2024-02-24 16:59:48 +0700

    Fix validation logic

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/userlottery/UserLotteryController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/userlottery/UserLotteryController.java
@@ -4,6 +4,7 @@ import com.kbtg.bootcamp.posttest.lottery.LotteryService;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,19 +22,19 @@ public class UserLotteryController {
     }
 
     @PostMapping("/{userId}/lotteries/{ticketId}")
-    public ResponseUserLotteryId buyUserLotteryByTicketId(@PathVariable Integer userId ,@Size(min = 6, max = 6) @PathVariable String ticketId) {
+    public ResponseUserLotteryId buyUserLotteryByTicketId(@Pattern(regexp = "\\d{10}") @PathVariable Integer userId ,@Pattern(regexp = "\\d{6}") @PathVariable String ticketId) {
         String id = userLotteryService.buyUserLotteryByTicketId(userId, ticketId);
         return new ResponseUserLotteryId(id);
     }
 
     @GetMapping("/{userId}/lotteries")
-    public ResponseUserLotteries getAllLotteryByUser(@PathVariable Integer userId) {
+    public ResponseUserLotteries getAllLotteryByUser(@Pattern(regexp = "\\d{10}") @PathVariable Integer userId) {
         UserLotteryDetail lotteriedDetail = userLotteryService.getAllLotteryByUser(userId);
         return new ResponseUserLotteries(lotteriedDetail.getTickets(),lotteriedDetail.getCount(),lotteriedDetail.getCost());
     }
 
     @DeleteMapping("/{userId}/lotteries/{ticketId}")
-    public ResponseLottery deleteUserLottery(@PathVariable Integer userId ,@Size(min = 6, max = 6) @PathVariable String ticketId) {
+    public ResponseLottery deleteUserLottery(@Pattern(regexp = "\\d{10}") @PathVariable Integer userId ,@Pattern(regexp = "\\d{6}") @PathVariable String ticketId) {
          userLotteryService.deleteUserLottery(userId, ticketId);
          return new ResponseLottery(ticketId);
     }
