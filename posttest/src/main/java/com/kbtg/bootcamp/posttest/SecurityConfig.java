9f160ac4839007b2db8928bc97a60123d2322bd5
chayanon <chayanon.mongkonhatairat@gmail.com>
2024-02-24 15:26:30 +0700

    Add admin authentication process

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.kbtg.bootcamp.posttest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Component
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defauSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/users/**").permitAll()
+                                .requestMatchers("/lotteries/**").permitAll()
+                                .anyRequest()
+                                .authenticated())
+                                .httpBasic(withDefaults()).
+                                build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+        @Bean
+    public AuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.withUsername("admin")
+                .password(new BCryptPasswordEncoder().encode("password"))
+                .roles("ADMIN").build();
+
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+
+    @Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
